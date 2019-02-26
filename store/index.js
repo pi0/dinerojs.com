7787b5c8fa70b135dd719dde50f334591366b286
Sarah Dayan <dayan.sarah@gmail.com>
2019-02-27 00:36:57 +0100

    Cleanup store

--- store/index.js
+++ store/index.js
@@ -1,37 +1,45 @@
 import axios from 'axios'
 
+const apiUrl = 'http://localhost:4000/docs/'
+const githubApiUrl = 'https://api.github.com/repos/sarahdayan/dinero.js/releases'
+
 export const state = () => ({
-  apiUrl: 'http://localhost:4000/docs/',
+  apiUrl,
   locale: 'en',
   changelog: []
 })
 
 export const getters = {
-  getLatestVersion: state => state.changelog[0].tag_name
+  latestVersion: state => state.changelog[0].tag_name,
+  routesFromEndpoints: state =>
+    Object.entries(state.endpoints).map(endpoint =>
+      endpoint[1].replace(
+        /(https?:\/\/[a-z0-9.]{1,}:[0-9]{1,}\/docs\/[a-z]{1,}\/)/,
+        ''
+      )
+    )
 }
 
 export const mutations = {
-  setApiUrl(state, apiUrl) {
-    state.apiUrl = apiUrl
-  },
-  setLocale(state, locale) {
-    state.locale = locale
-  },
   setChangelog(state, changelog) {
     state.changelog = changelog
+  },
+  setEndpoints(state, endpoints) {
+    state.endpoints = endpoints
   }
 }
 
 export const actions = {
   async nuxtServerInit({ commit }) {
-    let res = null
+    let changelog = null
+    let endpoints = null
     try {
-      res = await axios.get(
-        'https://api.github.com/repos/sarahdayan/dinero.js/releases'
-      )
+      changelog = await axios.get(githubApiUrl)
+      endpoints = await axios.get(apiUrl)
     } catch (err) {
       throw err
     }
-    commit('setChangelog', res.data)
+    commit('setChangelog', changelog.data)
+    commit('setEndpoints', endpoints.data)
   }
 }
